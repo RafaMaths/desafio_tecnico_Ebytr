8d47c81a2143b09a33942c8591581ed3d0a42a6a
Rafael <rmathscoach@gmail.com>
2021-11-03 18:49:33 -0300

    importação das funções do controller para a rota

--- backEnd/src/routers/taskRouter.js
+++ backEnd/src/routers/taskRouter.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const {
+  createTask_Control,
+  getAllTask_Control,
+  getTaskById_Control,
+  updateTask_Control,
+  deleteTask_Control
+} = require('../controllers/taskController')
 
 const router = express.Router();
 
-router.post('/');
-router.get('/');
-router.put('/')
-router.delete('/');
+router.post('/', createTask_Control);
+router.get('/', getAllTask_Control);
+router.get('/', getTaskById_Control);
+router.put('/', updateTask_Control)
+router.delete('/', deleteTask_Control);
 
 module.exports = router;
