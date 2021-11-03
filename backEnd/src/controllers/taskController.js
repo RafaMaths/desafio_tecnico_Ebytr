3468021793f3666be80bbe31d2f2a730fb714c31
Rafael <rmathscoach@gmail.com>
2021-11-03 18:34:09 -0300

    crio a função getAll no controller

--- backEnd/src/controllers/taskController.js
+++ backEnd/src/controllers/taskController.js
@@ -1,16 +1,22 @@
 const CODE = require('http-status-codes');
-const {createTask} = require('../models/taskModel');
+const {createTask, getAllTask} = require('../models/taskModel');
 
-const createTask = async (req, res) => {
+const createTask_Control = async (req, res) => {
   const {task, statusTask } = req.body;
 
-  const task = await createTask({task, statusTask});
+  const taskCreated = await createTask({task, statusTask});
 
-  return res.status(CODE.CREATED).json({task});
+  return res.status(CODE.CREATED).json(taskCreated);
 };
 
+const getAllTask_Control = async (req, res) => {
+  const allTasks = await getAllTask();
+  return res.status(CODE.OK).json(allTasks);
+}
+
 module.exports = {
-  createTask
+  createTask_Control,
+  getAllTask_Control
 }
 
 
